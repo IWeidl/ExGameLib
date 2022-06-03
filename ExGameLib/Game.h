40253e99733b762a0b49391b6df93f62f2a06da7
Isaiah Weidl <2137144+IWeidl@users.noreply.github.com>
2022-06-03 16:03:53 +1000

    Commenting and refactoring.

--- ExGameLib/Game.h
+++ ExGameLib/Game.h
@@ -12,17 +12,23 @@ namespace EGL
 	public:
 		Game();
 		void Run();
-		void Update();
-		void Draw();
+		
 	private:
 		sf::RenderWindow gameWindow;
 		sf::Clock clock;
 		int frameRate = 60;
 		float dt;
-		std::unordered_map<json, json> entities;
 		entt::registry registry;
+		/// <summary>
+		/// Loads all the entities and their components from a file and adds them to the ECS registry.
+		/// </summary>
+		/// <param name="fileName">Path to the file.</param>
 		void LoadEntities(std::string fileName);
+		/// <summary>
+		/// For each Graphics component, load the texture from the texturePath field and then allocate that to the sprite.
+		/// </summary>
 		void LoadTextures();
-
+		void Update();
+		void Draw();
 	};
 }
