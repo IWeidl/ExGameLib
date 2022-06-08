d1585616c9257292e044d318ab59f557078b14f1
Isaiah Weidl <2137144+IWeidl@users.noreply.github.com>
2022-06-08 14:44:28 +1000

    Commenting,

--- ExGameLib/ScriptManager.h
+++ ExGameLib/ScriptManager.h
@@ -30,7 +30,17 @@ namespace EGL {
 		/// <param name="x"></param>
 		/// <param name="y"></param>
 		void s_Move(const std::string& entityName, const float& x, const float& y);
+		/// <summary>
+		/// Sets an enity's location (x, y).
+		/// </summary>
+		/// <param name="entityName"></param>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
 		void s_SetPos(const std::string& entityName, const float& x, const float& y);
+		/// <summary>
+		/// Deletes all entities with entityName.
+		/// </summary>
+		/// <param name="entityName"></param>
 		void s_DeleteEntity(const std::string& entityName);
 	};
 
